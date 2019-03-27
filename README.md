3bdcd22583a2f0008673c844e7bc2fdfb1151a4c
gpaulbr <gpaulbr@gmail.com>
2019-03-27 16:20:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Woop Sicredi Events
+	This repository is a development test for Woop Sicredi iOS job aplication
---
+++ WoopSicrediEvents/WoopSicrediEvents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WoopSicrediEvents
+//
+//  Created by Gabriel Paul on 27/03/19.
+//  Copyright © 2019 GP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
